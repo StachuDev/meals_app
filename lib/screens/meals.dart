4cb375c0fb4f9dd342fa3309b26e85416088b583
StachuDev <solszakdev@gmail.com>
2024-03-18 13:16:36 +0100

    meal item widget with its own traits

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -14,13 +15,9 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-        itemCount: meals.length,
-        itemBuilder: ((context, index) {
-          return Text(
-            meals[index].title,
-            style: const TextStyle(color: Colors.white),
-          );
-        }));
+      itemCount: meals.length,
+      itemBuilder: ((context, index) => MealItem(meal: meals[index])),
+    );
 
     if (meals.isEmpty) {
       content = Center(
