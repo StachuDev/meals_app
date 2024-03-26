f829c8b434166905b507066dbfc493af58736a8b
StachuDev <solszakdev@gmail.com>
2024-03-26 11:00:46 +0100

    transfering and setting filteredMeals to providers

--- lib/providers/filters_providers.dart
+++ lib/providers/filters_providers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/meals_provider.dart';
 
 enum Filter {
   lactoseFree,
@@ -34,3 +35,24 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
 final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
         (ref) => FiltersNotifier());
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final selectedFilters = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (selectedFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/providers/favorites_provider.dart';
-import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
@@ -43,23 +42,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final selectedFilters = ref.watch(filtersProvider);
-    final dummyMealsFiltered = meals.where((meal) {
-      if (selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (selectedFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final dummyMealsFiltered = ref.watch(filteredMealsProvider);
     Widget activePage = CategoriesScreen(
       availableMeals: dummyMealsFiltered,
     );
